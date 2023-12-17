b59d08ed6261a4dd2a5e229020f94fb10608e73d
makst <maks.tierientiev00@gmail.com>
2023-12-17 15:50:16 +0300

    5

--- src/main/java/com/example/SpringPostgres/MainCotnr.java
+++ src/main/java/com/example/SpringPostgres/MainCotnr.java
@@ -2,6 +2,7 @@ package com.example.SpringPostgres;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -9,7 +10,7 @@ import java.time.LocalTime;
 @Controller
 public class MainCotnr {
     @GetMapping("/")
-    public String a(){
+    public @ResponseBody String a(){
         return LocalTime.now().toString();
     }
 }
